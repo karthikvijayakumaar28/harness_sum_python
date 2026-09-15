942930acb7697f6e399f8d21ceb6e553bc7234c9
Karthik Vijayakumaar <karthikvijayakumaar28@gmail.com>
2026-09-15 22:30:14 +0530

    Update DRONE_OUTPUT handling in sum.py
    Change the way DRONE_OUTPUT environment variable is retrieved and modify file opening mode.

--- sum.py
+++ sum.py
@@ -11,7 +11,6 @@ print("B value is", B)
 print(f"Result: {RESULT}")
 
 # Export RESULT for Harness
-env_file = os.environ["DRONE_OUTPUT"]
-
-with open(env_file, "a") as f:
+env_file = os.getenv("DRONE_OUTPUT", "/tmp/output")
+with open(env_file, "w") as f:
     f.write(f"RESULT={RESULT}\n")
