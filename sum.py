1fba4588d15ba4c19cd1f80edae1c932e11a5d67
Karthik Vijayakumaar <karthikvijayakumaar28@gmail.com>
2026-09-15 22:09:26 +0530

    Change file write mode from 'w' to 'a' for output

--- sum.py
+++ sum.py
@@ -11,6 +11,7 @@ print("B value is", B)
 print(f"Result: {RESULT}")
 
 # Export RESULT for Harness
-env_file = os.getenv("DRONE_OUTPUT", "/tmp/output")
-with open(env_file, "w") as f:   # use "w" instead of "a"
+env_file = os.environ["DRONE_OUTPUT"]
+
+with open(env_file, "a") as f:
     f.write(f"RESULT={RESULT}\n")
